916967e649dee8cbcd1bff9eeecb344100fb001d
mw <minwook0523@gmail.com>
2023-08-23 01:01:35 +0900

    시작페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './Page/Main';
+import StartPage from './Page/StartPage';
 import Test from './Components/Test';
 import Test2 from './Components/Test2';
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Test />}/>
+          <Route path='/' element={<StartPage />}/>
           <Route path='/test' element={<Test2 />}/>
           <Route path='/main' element={<Main />}/>
         </Routes>
--- src/Page/StartPage.tsx
+++ src/Page/StartPage.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import '../style/startPage.scss'
 const StartPage = () => {
   return (
-    <div>
-      
+    <div className='StartPage'>
+      <div className="pageTop"></div>
+      <div className="pageMain">
+        <div className="left">
+          left
+        </div>
+        <div className="right">
+          
+        </div>
+      </div>
+      <div className="pageTop"></div>
     </div>
   )
 }
