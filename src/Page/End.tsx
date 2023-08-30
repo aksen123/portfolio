e560ae39c140959f4eb79498ae662fd03847543e
mw <minwook0523@gmail.com>
2023-08-30 17:18:54 +0900

    end page

--- src/Page/End.tsx
+++ src/Page/End.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import '../style/startPage.scss'
 import Logo from '../Components/logo/Logo'
+import { useNavigate } from 'react-router'
 const End = () => {
+  const navigate = useNavigate()
+  setTimeout(()=>{
+    navigate('/contact')
+  },2000)
   return (
     <div className='End'>
       <Logo />
